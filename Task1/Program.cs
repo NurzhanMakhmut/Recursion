04ade20adc7e8f9508d6b6787c560e2e19a36ab8
Nurzhan_Makhmut <87759046037b@gmail.com>
2024-02-12 23:23:47 +0600

    добавили задачи

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
+Console.Write("Введите начальное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите начальное число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+void ShowNumber (int start, int end)
+{
+    if(start==end)
+    {
+        Console.Write(start);
+        return;
+    }
+    Console.Write(start + " ");
+    ShowNumber(start+1,end);
+}
+ShowNumber(N,M);
---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+// Семинар 7. Рекурсия
+// Пример
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+int[] array = {1, 2, 5, 0, 10, 34};
+int size = array.Length;
+void ShowArray (int[] arr,int size)
+{
+    
+    if (size==0)
+    {
+        return;
+    }
+    Console.Write(arr[size-1]+" ");
+
+    ShowArray (arr,size-1);
+}
+   ShowArray (array,size);
